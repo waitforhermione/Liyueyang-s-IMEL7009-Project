939fe6ef6d6ebd2585214231e19d05207d1a128f
leslie <56627460+waitforhermione@users.noreply.github.com>
2024-02-01 21:42:54 +0800

    main.py

--- main.py
+++ main.py
@@ -13,54 +13,54 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 def fc_forward(z, W, b):
     """
-    全连接层的前向传播
-    :param z: 当前层的输出,形状 (N,ln)
-    :param W: 当前层的权重
-    :param b: 当前层的偏置
-    :return: 下一层的输出
+    The forward propagation of a fully connected layer
+    :param z: Output of this layer, shape: (N,ln)
+    :param W: Weight of this layer,
+    :param b: Bias of this layer,
+    :return:  Output of next layer,
     """
     return np.dot(z, W) + b
 def fc_backward(next_dz, W, z):
     """
-    全连接层的反向传播
-    :param next_dz: 下一层的梯度
-    :param W: 当前层的权重
-    :param z: 当前层的输出
+    The backward propagation of a fully connected layer
+    :param next_dz: gradients of next layer,
+    :param W: gradients of this layer,
+    :param z: output of this layer,
     :return:
     """
     N = z.shape[0]
-    dz = np.dot(next_dz, W.T)  # 当前层的梯度
-    dw = np.dot(z.T, next_dz)  # 当前层权重的梯度
-    db = np.sum(next_dz, axis=0)  # 当前层偏置的梯度, N个样本的梯度求和
+    dz = np.dot(next_dz, W.T)
+    dw = np.dot(z.T, next_dz)
+    db = np.sum(next_dz, axis=0)
     return dw / N, db / N, dz
 def relu_forward(z):
     """
-    relu前向传播
-    :param z: 待激活层
-    :return: 激活后的结果
+    ReLU forward propagation
+    :param z: layer before activation
+    :return: activated result
     """
     return np.maximum(0, z)
 def relu_backward(next_dz, z):
     """
-    relu反向传播
-    :param next_dz: 激活后的梯度
-    :param z: 激活前的值
+    ReLU backward propagation
+    :param next_dz: activated gradient
+    :param z: value before activation
     :return:
     """
     dz = np.where(np.greater(z, 0), next_dz, 0)
     return dz
 def cross_entropy_loss(y_predict, y_true):
     """
-    交叉熵损失函数
-    :param y_predict: 预测值,shape (N,d)，N为批量样本数
-    :param y_true: 真实值,shape(N,d)
+    cross-entropy loss function
+    :param y_predict: prediction,shape (N,d)，
+    :param y_true: true value,shape(N,d)
     :return:
     """
 
     y_shift = y_predict - np.max(y_predict, axis=-1, keepdims=True)
     y_exp = np.exp(y_shift)
     y_probability = y_exp / np.sum(y_exp, axis=-1,keepdims=True)
-    loss = np.mean(np.sum(-y_true * np.log(y_probability), axis=-1))  # 损失函数
+    loss = np.mean(np.sum(-y_true * np.log(y_probability), axis=-1))  # loss function
     dy = y_probability - y_true
     return loss, dy
 
@@ -72,7 +72,7 @@ def forward(X):
     nuerons["z3_relu"]=relu_forward(nuerons["z3"])
     nuerons["y"]=fc_forward(nuerons["z3_relu"],weights["W3"],weights["b3"])
     return nuerons["y"]
-# 定义后向过程
+
 def backward(X,y_true):
     loss,dy=cross_entropy_loss(nuerons["y"],y_true)
     gradients["W3"],gradients["b3"],gradients["z3_relu"]=fc_backward(dy,weights["W3"],nuerons["z3_relu"])
@@ -105,7 +105,7 @@ def load_mnist_datasets(path='mnist.pkl.gz'):
 
 
 def to_categorical(y, num_classes=None):
-    """从keras中复制而来
+    """
     Converts a class vector (integers) to binary class matrix.
 
     E.g. for use with categorical_crossentropy.
@@ -148,17 +148,13 @@ nuerons={}
 gradients={}
 train_set, val_set, test_set = load_mnist_datasets('mnist.pkl.gz')
 train_y,val_y,test_y=to_categorical(train_set[1]),to_categorical(val_set[1]),to_categorical(test_set[1])
-#随机选择训练样本
+#Randomly selecting training samples
 train_num = train_set[0].shape[0]
 def next_batch(batch_size):
     idx=np.random.choice(train_num,batch_size)
     return train_set[0][idx],train_y[idx]
 x,y= next_batch(16)
 print("x.shape:{},y.shape:{}".format(x.shape,y.shape))
-
-"""digit=train_set[0][3]
-plt.imshow(np.reshape(digit,(28,28)))
-plt.show()"""
 
 # 初始化变量
 batch_size = 32
@@ -168,13 +164,13 @@ lr = 0.1
 
 for e in range(epoch):
     for s in range(steps):
-        X, y = next_batch(batch_size)#返回了训练用的输入X，和其真值y
+        X, y = next_batch(batch_size)
 
-        # 前向过程
+        # forward and backward
         forward(X)
         loss = backward(X, y)
 
-        # 更新梯度
+        # undate gradients
         for k in ["W1", "b1", "W2", "b2", "W3", "b3"]:
             weights[k] -= lr * gradients[k]
 
